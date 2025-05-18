df7ec82612999a1b239aa708f48aca19aaa2d930
Traii Mohamed Amin <99541159+themedworld@users.noreply.github.com>
2025-05-18 17:32:52 +0100

    Update chatboot.tsx

--- src/app/Component/chatboot.tsx
+++ src/app/Component/chatboot.tsx
@@ -42,7 +42,7 @@ const scrollToBottom = () => {
   setIsLoading(true);
 
   try {
-    const res = await fetch("https://ton-api-backend.onrender.com/recommend", {
+    const res = await fetch("https://backendhotelrec-1.onrender.com", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({ message: userMessage })
